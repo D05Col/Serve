6111abc612f5c0c7aa22d08182150bb94db1acbb
Dylan Collins <djdylan7705@gmail.com>
2025-01-11 14:20:34 +0000

    finds the file and returns the file contents but sometimes reads invalid utf8 and panics

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,53 @@
 use std::io::{BufRead, BufReader, prelude::*};
-use std::net::{TcpListener, TcpStream};
+use std::net::{Shutdown, TcpListener, TcpStream};
 use std::fs;
 use std::path::Path;
+use std::env;
+use std::env::current_dir;
 
 fn main() {
     // Use 7878 as it shouldn't conflict with anything else running locally
     println!("Listening for connections...");
+    println!("{:?}", current_dir());
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    handle_connection(listener);
-}
-
-fn handle_connection(listener: TcpListener) {
-    // Iterating the streams like this is synchronous for now
     for stream in listener.incoming() {
-        // The way that the tcp listener works is it queues connection attempts trying to connect
-        // This tcp stream will be open to read the request and write the response following HTTP formats
         let stream = stream.unwrap();
-        // TODO: TcpListener bind returns a result. We need failure handling instead of just unwrapping
-        println!("Connection established!");
+        println!("Connection Attempt Received!");
         process_request(stream);
-
     }
 }
 
 fn process_request(mut stream: TcpStream) {
-    // Function to return the resource requested
-
     // Request can be quite large so read the contents in a bufReader to remove read overhead
     let buffer = BufReader::new(&stream);
 
     // Need to only take lines that are valid UTF-8 otherwise we panic. (empty is not UTF-8)
-    let request: Vec<_>  = buffer.lines().map(|line| line.unwrap()).collect();//.take_while(|content| !content.is_empty()).collect();
+    let request = buffer.lines().next().unwrap().unwrap();
 
-    // Get the request type, resource and protocol
-    let important_request_information = request[0].clone();
-    let important_elements = important_request_information.split(" ").collect::<Vec<&str>>();
+    let important_elements = request.split(" ").collect::<Vec<&str>>();
 
-    let request_type = important_elements[0];
-    let resource = important_elements[0];
-    let protocol = important_elements[0];
+    let request_type = important_elements[0].to_string();
+    let resource = important_elements[1].to_string();
+    let protocol = important_elements[2].to_string();
 
-    let file = Path::new(resource);
+    let mut environment_dir = current_dir().unwrap().as_path().to_str().unwrap().to_string();
+    let file_path_string = environment_dir + "\\src\\" + &resource[1..];
+    let file_path = Path::new(&file_path_string);
+    println!("{:?}", resource);
+    println!("{:?}", file_path);
 
-    let mut response;
+    //let mut response;
+
+    let response;
     // Generate a response using the standard http format
-    if file.exists(){
-        let file_contents = fs::read_to_string(file).unwrap();
+    if file_path.exists(){
+        let file_contents = fs::read_to_string(file_path).unwrap();
         response = generate_success_response(file_contents);
     }
     else{
         response = generate_not_found_response();
     }
+    println!("{response}");
     stream.write_all(response.as_bytes()).unwrap();
 }
 
@@ -58,10 +55,10 @@ fn generate_success_response(file_contents: String) -> String {
     // Not much functionality for now
     let length = file_contents.chars().count();
     let status_line = "HTTP/1.1 200 OK";
-    String::from(format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{file_contents}"))
+    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{file_contents}")
 }
 
 fn generate_not_found_response() -> String {
     let status_line = "HTTP/1.1 404 Not Found";
-    String::from(format!("{status_line}"))
+    format!("{status_line}")
 }
